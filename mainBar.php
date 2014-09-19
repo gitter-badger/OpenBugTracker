2ce526b5efe6b62c77de3074a6033f9193204807
Hugo Rodrigues <hugaomrodrigues@gmail.com>
2014-09-19 03:36:31 +0100

    Main bar design almost complete

--- mainBar.php
+++ mainBar.php
@@ -12,7 +12,22 @@
  */
 ?>
 <div id="mainBar">
-	<span id="projectMenu"></span>
-	<span id="logo"></span>
-	<div id="menu"></div>
+	<div id="header">
+		<a href="#" id="logo"><img src="">_</a>
+		<div id="search">
+			<form action="search.php" method="POST">
+				<input type="search" value="search">
+			</form>
+		</div>
+		<ul id="mainMenu">
+			<li><a href="projects.php">Projects</a></li>
+			<li><a href="hotBugs.php">Hot bugs</a></li>
+		</ul>
+		<ul id="userMenu">
+			<li href="#" id="privateMessage" title="Private Messages"><img src="images/resources/icons/message.png"></li>
+			<li href="#" id="notification" title="You don't have new notifications"><img src="images/resources/icons/notifications.png"></li>
+			<li href="#" id="userControl"><img src="images/resources/icons/guest.png"> <span>Guest</span></li>
+			<li href="#"id="logOut" title="Logout"><img src="images/resources/icons/logout.png"></li>
+		</ul>
+	</div>
 </div>
