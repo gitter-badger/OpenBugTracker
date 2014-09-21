7a2e81fc546e71d964d29b57af4d34f0b7563123
Hugo Rodrigues <hugaomrodrigues@gmail.com>
2014-09-21 17:40:14 +0100

    Footer template done

--- footer.php
+++ footer.php
@@ -12,6 +12,22 @@
  */
 ?>
 <div id="footer">
-	<div class="containter">
+	<div class="container">
+		<div id="serviceInfo">
+			<ul>
+				<li>Homepage</li> - 
+				<li>Terms of service</li> - 
+				<li>Contact</li> - 
+				<li>Status</li> - 
+				<li>Help</li> - 
+				<li>License</li> - 
+				<li>About</li>
+			</ul>
+		</div>
+		<div id="obtInfo">
+			Provided by<br>
+			<a href="https://github.com/Hugao/OpenBugTracker" target="_blank"><img src="images/obt.png"></a>
+		</div>
+		<div id="copyright">&copy; 2014 <a href="https://web-hugorodrigues.rhcloud.com/" target="_blank">Hugo Rodrigues</a> and <a href="https://github.com/Hugao/OpenBugTracker/graphs/contributors" target="_blank">contributors</a></div>
 	</div>
 </div>
