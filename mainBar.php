1e3bee1c5f87971f3bc6935d3d56fa18f7a45b8d
Hugo Rodrigues <hugaomrodrigues@gmail.com>
2014-09-21 14:01:57 +0100

    Remove pointer change when on hover and add link to message and notification icons

--- mainBar.php
+++ mainBar.php
@@ -25,8 +25,8 @@ $searchDefault = 'Search';
 			<li><a href="hotBugs.php">Hot bugs</a></li>
 		</ul>
 		<ul id="userMenu">
-			<li href="#" id="privateMessage" title="Private Messages"><img src="images/resources/icons/message.png" onmouseover="imgSrcChange(this, 'images/resources/icons/message_hover.png')" onmouseout="imgSrcChange(this, 'images/resources/icons/message.png')"></li>
-			<li href="#" id="notification" title="You don't have new notifications"><img src="images/resources/icons/notifications.png" onmouseover="imgSrcChange(this, 'images/resources/icons/notifications_hover.png')" onmouseout="imgSrcChange(this, 'images/resources/icons/notifications.png')"></li>
+			<li href="#" id="privateMessage" title="Private Messages"><a href="#"><img src="images/resources/icons/message.png" onmouseover="imgSrcChange(this, 'images/resources/icons/message_hover.png')" onmouseout="imgSrcChange(this, 'images/resources/icons/message.png')"></a></li>
+			<li href="#" id="notification" title="You don't have new notifications"><a href="#"><img src="images/resources/icons/notifications.png" onmouseover="imgSrcChange(this, 'images/resources/icons/notifications_hover.png')" onmouseout="imgSrcChange(this, 'images/resources/icons/notifications.png')"></a></li>
 			<li href="#" id="userControl"><a href="#"><img src="images/resources/icons/guest.png"> <span>Guest</span></a></li>
 			<li href="#" id="logOut" title="Logout"><a href="#"><img src="images/resources/icons/logout.png" onmouseover="imgSrcChange(this, 'images/resources/icons/logout_hover.png')" onmouseout="imgSrcChange(this, 'images/resources/icons/logout.png')"></a></li>
 		</ul>
