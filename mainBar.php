0e3abff6ba883ed71f672e4a80c89a03f7f5e9e1
Hugo Rodrigues <hugorodrigues.developer@outlook.com>
2014-10-27 11:05:06 +0000

    Replace ID by CLASS in transitions

--- mainBar.php
+++ mainBar.php
@@ -18,26 +18,26 @@ $searchDefault = 'Search';
 	<div class="container">
 		<div class="left">
 			<!-- Platform logo -->
-			<a href="<?php echo $config_baseUrl; ?>" id="logo"><img src="images/logo.png"></a>
+			<a href="<?php echo $config_baseUrl; ?>" id="logo" class="transOpacity02"><img src="images/logo.png"></a>
 			<!-- Platform main menu -->
 			<ul id="mainMenu">
-				<li><a href="projects.php">Projects</a></li>
-				<li><a href="myBugs.php">My Bugs</a></li>
+				<li><a class="transColor02" href="projects.php">Projects</a></li>
+				<li><a class="transColor02" href="myBugs.php">My Bugs</a></li>
 			</ul>
 		</div>
 		<div class="right">
 			<!-- Search bar -->
 			<div id="search">
 				<form action="search.php" method="POST">
-					<input type="search" id="searchBox" onclick="enterSearch('<?php echo $searchDefault; ?>')" onblur="exitSearch('<?php echo $searchDefault; ?>')" value="<?php echo $searchDefault; ?>">
+					<input class="transColor02" type="search" id="searchBox" onclick="enterSearch('<?php echo $searchDefault; ?>')" onblur="exitSearch('<?php echo $searchDefault; ?>')" value="<?php echo $searchDefault; ?>">
 				</form>
 			</div>
 			<!-- Platform user menu, is where controls relative to the user are displayed -->
 			<ul id="userMenu">
-				<li href="#" id="privateMessage" title="Private Messages"><a href="#"><img src="images/resources/icons/message.png"></a></li>
-				<li href="#" id="notification" title="You don't have new notifications"><a href="#"><img src="images/resources/icons/notifications.png"></a></li>
-				<li href="#" id="userControl"><a href="#"><img src="images/resources/icons/guest.png"> <span>Guest</span></a></li>
-				<li href="#" id="logOut" title="Logout"><a href="#"><img src="images/resources/icons/logout.png"></a></li>
+				<li href="#" id="privateMessage" class="transOpacity02" title="Private Messages"><a href="#"><img src="images/resources/icons/message.png"></a></li>
+				<li href="#" id="notification" class="transOpacity02" title="You don't have new notifications"><a href="#"><img src="images/resources/icons/notifications.png"></a></li>
+				<li href="#" id="userControl"><a class="transColor02" href="#"><img src="images/resources/icons/guest.png"> <span>Guest</span></a></li>
+				<li href="#" id="logOut" class="transOpacity02" title="Logout"><a href="#"><img src="images/resources/icons/logout.png"></a></li>
 			</ul>
 		</div>
 	</div>
